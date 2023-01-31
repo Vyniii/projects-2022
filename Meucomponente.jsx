a0e6de588deb86a9ef290a72be93962495d1cc06
Vinicius <123830537+Vyniii@users.noreply.github.com>
2023-01-31 20:52:58 -0300

    Add files via upload

---
+++ Meucomponente.jsx
@@ -0,0 +1,10 @@
+import "../styles/meucomponente.css"
+
+export default function Meucomponente(){
+    return(
+        <div className="meucomponente">
+            <h2>meu componente 2022</h2>
+            <p>-vynii</p>
+        </div>
+    );
+}
